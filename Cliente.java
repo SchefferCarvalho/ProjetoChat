476c8873bf0e209ec49b9dadb1ee005267f7fa96
Scheffer <scheffer.carvalho@gmail.com>
2019-04-22 17:03:16 -0300

    Chat com interface grafica funcionando com mensagem privada e geral

--- Cliente.java
+++ Cliente.java
@@ -20,7 +20,7 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 
     // parte que controla a recepção de mensagens do cliente
     private Socket conexao;
-    
+
     private static final long serialVersionUID = 1L;
     private JTextArea texto;
     private JTextField txtMsg;
@@ -28,6 +28,7 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
     private JButton btnSair;
     private JLabel lblHistorico;
     private JLabel lblMsg;
+    private JLabel lblPrivado;
     private JPanel pnlContent;
     private Socket socket;
     private OutputStream ou;
@@ -37,26 +38,29 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
     private JTextField txtPorta;
     private JTextField txtNome;
     private boolean continuar = true;
-    
+
     public Cliente() throws IOException {
         this.conexao = socket;
-        JLabel lblMessage = new JLabel("Verificar");
+        JLabel lblMessage1 = new JLabel("IP do Servidor");
+        JLabel lblMessage2 = new JLabel("Porta");
+        JLabel lblMessage3 = new JLabel("Nome do Usuário");
         txtIP = new JTextField("127.0.0.1");
         txtPorta = new JTextField("5555");
         txtNome = new JTextField("Cliente A");
-        Object[] texts = {lblMessage, txtIP, txtPorta, txtNome};
+        Object[] texts = {lblMessage1, txtIP, lblMessage2, txtPorta, lblMessage3, txtNome};
         JOptionPane.showMessageDialog(null, texts);
         pnlContent = new JPanel();
-        texto = new JTextArea(24, 40);
+        texto = new JTextArea(24, 45);
         texto.setEditable(false);
         texto.setBackground(new Color(240, 240, 240));
-        txtMsg = new JTextField(20);
+        txtMsg = new JTextField(25);
         lblHistorico = new JLabel("Histórico");
         lblMsg = new JLabel("Mensagem");
         btnSend = new JButton("Enviar");
         btnSend.setToolTipText("Enviar Mensagem");
         btnSair = new JButton("Sair");
         btnSair.setToolTipText("Sair do Chat");
+        lblPrivado = new JLabel("OBS: Para mandar uma mensagem privada digite '/' após a mensagem e o nome do Usuário");
         btnSend.addActionListener(this);
         btnSair.addActionListener(this);
         btnSend.addKeyListener(this);
@@ -69,6 +73,7 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
         pnlContent.add(txtMsg);
         pnlContent.add(btnSair);
         pnlContent.add(btnSend);
+        pnlContent.add(lblPrivado);
         pnlContent.setBackground(Color.LIGHT_GRAY);
         texto.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
         txtMsg.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
@@ -77,11 +82,11 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
         //setLocationRelativeTo(null);
         setLocation(700, 250);
         setResizable(false);
-        setSize(500, 500);
+        setSize(535, 515);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
+
     public void conectar() {
         try {
             socket = new Socket(txtIP.getText(), Integer.parseInt(txtPorta.getText()));
@@ -90,44 +95,49 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
             bfw = new BufferedWriter(ouw);
             bfw.write(txtNome.getText() + "\r\n");
             bfw.flush();
-        } catch (Exception e) {
-            System.exit(0);
+        } catch (IOException e) {
+            texto.append(" Servidor fora do ar ou não encontrado! \r\n");
+            continuar = false;
         }
     }
-    
+
     public void enviarMensagem(String msg) throws IOException {
         if (continuar) {
             if (msg != null && !(msg.trim().equals(""))) {
                 bfw.write(msg + "\r\n");
-                texto.append(txtNome.getText() + " - " + txtMsg.getText() + "\r\n");
+                texto.append(" " + txtNome.getText() + " - " + txtMsg.getText() + "\r\n");
                 bfw.flush();
                 txtMsg.setText("");
             }
         }
     }
-    
+
     public void escutar() throws IOException {
-        InputStream in = socket.getInputStream();
-        InputStreamReader inr = new InputStreamReader(in);
-        BufferedReader bfr = new BufferedReader(inr);
-        String msg = "";
-        while (continuar) {
-            if (bfr.ready()) {
-                msg = bfr.readLine();
-                texto.append(msg + "\r\n");
+        if (continuar) {
+            InputStream in = socket.getInputStream();
+            InputStreamReader inr = new InputStreamReader(in);
+            BufferedReader bfr = new BufferedReader(inr);
+            String msg = "";
+            while (continuar) {
+                if (bfr.ready()) {
+                    msg = bfr.readLine();
+                    texto.append(" " + msg + "\r\n");
+                }
             }
         }
     }
-    
+
     public void sair() throws IOException {
-        continuar = false;
-        bfw.close();
-        ouw.close();
-        ou.close();
-        socket.close();
+        if (continuar) {
+            continuar = false;
+            bfw.close();
+            ouw.close();
+            ou.close();
+            socket.close();
+        }
         System.exit(0);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
@@ -139,12 +149,12 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
         } catch (IOException ex) {
             // TODO Auto-generated catch block
             // ex.printStackTrace();
-            texto.append("Servidor fora do ar! \r\n");
+            texto.append(" Servidor fora do ar ou não encontrado! \r\n");
             continuar = false;
             //System.exit(0);
         }
     }
-    
+
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
@@ -153,23 +163,23 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
             } catch (IOException ex) {
                 // TODO Auto-generated catch block
                 //ex.printStackTrace();
-                texto.append("Servidor fora do ar! \r\n");
+                texto.append(" Servidor fora do ar ou não encontrado! \r\n");
                 continuar = false;
                 //System.exit(0);
             }
         }
     }
-    
+
     @Override
     public void keyReleased(KeyEvent arg0) {
         // TODO Auto-generated method stub               
     }
-    
+
     @Override
     public void keyTyped(KeyEvent arg0) {
         // TODO Auto-generated method stub               
     }
-    
+
     public static void main(String[] args) throws IOException {
         Cliente app = new Cliente();
         app.conectar();
--- Servidor.java
+++ Servidor.java
@@ -1,7 +1,6 @@
 package ProjetoChat;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -11,23 +10,26 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.io.PrintStream;
-import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
+import java.util.Map;
 
+//import java.util.Vector;
 public class Servidor extends Thread {
 
-    private static ArrayList<BufferedWriter> clientes;
+    //private static ArrayList<BufferedWriter> clientes;
     private static ServerSocket server;
 
     // Parte que controla as conexões por meio de threads.
-    private static Vector CLIENTES;
+    private static Map<String, PrintStream> MAP_CLIENTES;
     // socket deste cliente
     private Socket conexao;
     // nome deste cliente
     private String nomeCliente;
     // lista que armazena nome de CLIENTES
     private static List LISTA_DE_NOMES = new ArrayList();
+
+    private PrintStream saida;
 
     // construtor que recebe o socket deste cliente
     public Servidor(Socket socket) {
@@ -57,8 +59,8 @@ public class Servidor extends Thread {
     }
 
     public static void main(String args[]) {
-        // instancia o vetor de CLIENTES conectados
-        CLIENTES = new Vector();
+        // instancia MAP_CLIENTES conectados
+        MAP_CLIENTES = new HashMap();
         try {
             //Cria os objetos necessário para instânciar o servidor
             JLabel lblMessage = new JLabel("Porta do Servidor:");
@@ -66,7 +68,7 @@ public class Servidor extends Thread {
             Object[] texts = {lblMessage, txtPorta};
             JOptionPane.showMessageDialog(null, texts);
             server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
-            clientes = new ArrayList<BufferedWriter>();
+            //clientes = new ArrayList<BufferedWriter>();
             JOptionPane.showMessageDialog(null, "Servidor ativo na porta: " + txtPorta.getText());
             System.out.println("Servidor ativo na porta - " + txtPorta.getText());
             // Loop principal.     
@@ -84,6 +86,7 @@ public class Servidor extends Thread {
 
         } catch (IOException e) {
             // caso ocorra alguma excessão de E/S, mostre qual foi.
+            System.out.println(" AQUI");
             System.out.println("IOException: " + e);
         }
     }
@@ -92,77 +95,85 @@ public class Servidor extends Thread {
     public void run() {
         try {
             // objetos que permitem controlar fluxo de comunicação que vem do cliente
-            BufferedReader entrada
-                    = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
-
-            PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            saida = new PrintStream(this.conexao.getOutputStream());
             // recebe o nome do cliente
             this.nomeCliente = entrada.readLine();
             //chamada ao metodo que testa nomes iguais
             if (armazena(this.nomeCliente)) {
                 saida.println("Este nome ja existe! Conecte novamente com outro Nome.");
-                CLIENTES.add(saida);
+                //CLIENTES.add(saida);
                 //fecha a conexao com este cliente
                 this.conexao.close();
                 return;
             } else {
                 //mostra o nome do cliente conectado ao servidor
                 System.out.println(this.nomeCliente + " - Conectado");
+                //Quando o cliente se conectar recebe todos que estão conectados
+                saida.println("Conectados: " + LISTA_DE_NOMES.toString());
             }
             //igual a null encerra a execução
             if (this.nomeCliente == null) {
                 return;
             }
-            //adiciona os dados de saida do cliente no objeto CLIENTES
-            CLIENTES.add(saida);
-            //recebe a mensagem do cliente
-            String msg = entrada.readLine();
+            //adiciona os dados de saida do cliente no objeto MAP_CLIENTES
+            //A chave será o nome e valor o printstream
+            MAP_CLIENTES.put(this.nomeCliente, saida);
+            String[] msg = entrada.readLine().split("/");
             // Verificar se linha é null (conexão encerrada)
             // Se não for nula, mostra a troca de mensagens entre os CLIENTES
-            while (msg != null && !(msg.trim().equals(""))) {
-                System.out.println(this.nomeCliente + " - " + msg);
+            while (msg != null) {
+                System.out.println(this.nomeCliente + " - " + msg[0]);
                 // reenvia a linha para todos os CLIENTES conectados
-                sendToAll(saida, " - ", msg);
+                send(saida, " - ", msg);
                 // espera por uma nova linha.
-                msg = entrada.readLine();
+                msg = entrada.readLine().split("/");
             }
-            //se cliente enviar linha em branco, mostra a saida no servidor
             System.out.println(this.nomeCliente + " - Desconectado");
             // mensagem de saida do chat aos CLIENTES conectados
-            sendToAll(saida, " - ", "Desconectado");
+            String[] out = {"Desconectado"};
+            send(saida, " - ", out);
             //remove nome da lista
             remove(this.nomeCliente);
             //exclui atributos setados ao cliente
-            CLIENTES.remove(saida);
+            MAP_CLIENTES.remove(this.nomeCliente);
             //fecha a conexao com este cliente
             this.conexao.close();
-        } catch (IOException e) {
-            // Caso ocorra alguma excessão de E/S, mostre qual foi.
-            //System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        } catch (Exception e) {
             //se cliente enviar linha em branco, mostra a saida no servidor
             System.out.println(this.nomeCliente + " - Desconectado");
             // mensagem de saida do chat aos CLIENTES conectados
-            Enumeration en = CLIENTES.elements();
-            while (en.hasMoreElements()) {
-                // obtém o fluxo de saída de um dos CLIENTES
-                PrintStream chat = (PrintStream) en.nextElement();
-                // envia para todos, menos para o próprio usuário
-                chat.println(this.nomeCliente + "- Desconectado");
+            for (Map.Entry cliente : MAP_CLIENTES.entrySet()) {
+                PrintStream chat = (PrintStream) cliente.getValue();
+                if (chat != saida) {
+                    chat.println(" " + this.nomeCliente + " - Desconectado");
+                }
             }
             //remove nome da lista
             remove(this.nomeCliente);
+            //exclui atributos setados ao cliente
+            MAP_CLIENTES.remove(this.nomeCliente);
         }
     }
 
-    // enviar uma mensagem para todos, menos para o próprio
-    public void sendToAll(PrintStream saida, String acao, String msg) throws IOException {
-        Enumeration e = CLIENTES.elements();
-        while (e.hasMoreElements()) {
-            // obtém o fluxo de saída de um dos CLIENTES
-            PrintStream chat = (PrintStream) e.nextElement();
-            // envia para todos, menos para o próprio usuário
+    /**
+     * Se o array da msg tiver tamanho igual a 1, então envia para todos Se o
+     * tamanho for 2, envia apenas para o cliente escolhido
+     */
+    public void send(PrintStream saida, String acao, String[] msg) throws IOException {
+        out:
+        for (Map.Entry cliente : MAP_CLIENTES.entrySet()) {
+            PrintStream chat = (PrintStream) cliente.getValue();
             if (chat != saida) {
-                chat.println(this.nomeCliente + acao + msg);
+
+                if (msg.length == 1) {
+                    chat.println(" " + this.nomeCliente + acao + msg[0]);
+                } else {
+                    if (msg[1].equalsIgnoreCase((String) cliente.getKey())) {
+                        chat.println(" " + this.nomeCliente + acao + msg[0]);
+                        break out;
+                    }
+                }
             }
         }
     }
